363a7546986179b2d4bb9b942480c2503c28c989
kursatoguz <kursatoguz07@hotmail.com>
2023-01-28 03:49:07 +0300

    redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import countriesReducer from "../features/countriesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    countries: countriesReducer,
   },
 });
